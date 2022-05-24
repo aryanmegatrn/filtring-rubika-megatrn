e9a9413b90f53c1511ac750e3ffbc5e95133883c
aryanmegatrn <105610770+aryanmegatrn@users.noreply.github.com>
2022-05-24 15:21:21 +0430

    Update filtring.py

--- filtring.py
+++ filtring.py
@@ -1,1 +1,198 @@
 
+port os
+print (" ♕ ARYAN MEGATRN ♕ ")
+# auto install libs
+os.system("pip install colored")
+os.system("pip install colorama")
+os.system("pip install datetime")
+#importing libs
+
+import time                                                     
+import sys                                                      
+import colored                                                  
+import colorama                                                 
+import datetime
+from colored import fg, bg, attr
+from colorama import Fore, Back, Style                          
+# code colors
+red='\033[31m'
+green='\033[32m'
+blue='\033[36m'
+pink='\033[35m'
+rang='\033[34m'
+#$CRIPT
+
+os.system("clear")
+
+print(f"{green} ")
+
+print (Fore.WHITE + "")
+
+#date()
+x = str(datetime.datetime.now())
+
+print("started " + (x))
+
+print(f"{blue} ")
+print ("         Script By")
+print (Fore.BLUE + "")
+x = """               .. ... ....                                               Mehrab
+Hello My Friend Im Aryan Megatrn I am filtering and I want to give you, my friends, a filtering code. I hope you use it correctly. Thank you.
+GOD FIltering Rubika >~Aryan Megatrn>_
+                          
+						    __   __
+								|  \/  |
+								| |\/| |
+								| |  | |
+								|_|  |_|
+								
+								 _____
+								| ____|
+								|  _|
+								| |___
+								|_____|
+								
+								  ____
+							  / ___|
+								| |  _
+								| |_| |
+								 \____|
+								 
+								    _        
+						    	 / \      
+								  / _ \    
+								 / ___ \  
+								/_/   \_\								
+								
+																	
+						  		 _____
+									|_   _|
+									  | |
+									  | |
+									  |_|
+
+					 			  ____
+									|  _ \
+									| |_) |
+									|  _ <
+									|_| \_\
+																		 			
+									 _   _
+									| \ | |
+									|  \| |
+									| |\  |
+									|_| \_|									 			
+									 			
+                     new~acont~rubika:@mi_aryan_megatrn
+                       id tel:@Mr_aryan_a_h_a
+"""
+for c in x:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.02)
+print ("")
+print(" US ")
+print("")
+print("")
+print("")
+print (" ")
+up = input("""
+          _   Acc 1 _
+            _   Chanel 2 _
+                   _  Gp 3 ~ _>> """ )
+print(f"{blue} ")
+
+
+
+if up == "1":
+
+        print ('Account  == ' + '%s%s (https://.lnpker.dlit.http//(reaprot.(81.23.40.7)_senotor.pro(+bit.ly/9ploftakhttps+×CFh) %s' % (fg('red'), bg('black'), attr('reset')))
+
+
+if up != "1":
+        print("    мy code ")
+
+
+if up == "2":
+        print (' Chanel Cod  == ' + '%s%s (http://3.py/F./h.k.p////6.4.(157.621/https://s4.uupload.ir/files/n5_j0e2.jpg)(.(/U/p.[157.6)l////8.9.0.2.4.7.9.3.1.5.2.7.8.3.0.1.0) %s' % (fg('white'), bg('black'), attr('reset')))
+
+
+if up != "2":
+    print("    My Cods ")
+
+if up == "3":
+
+        print (' GP Cod ==' + '%a%  (4.0.7.5/e////fg/5.2.7.9.0.2.3.1.3.4.5.5.2.1.5.7.2.7.8.6.9.9.0.1/) %a' % (fg('red'), bg('black'), attr('reset')))
+
+
+
+if up != "3":
+        print("  мy code ")
+        
+        
+time.sleep(1)
+print(f"{pink} ")
+time.sleep(0.3)
+print (" Cod Ro 10 bar Bezanin Lotfa Va Dorost Cod Ro Bezanin Ta Fake Nasheh ")
+print(" ")
+print (" ")
+print (" Tank You ")
+time.sleep(0.6)
+print(f"{green}" )
+d = """
+Im Aryan Megatrn 
+Id Rubika: @megatrn
+Chanel Rubika: @mi_aryan_megatrn
+Id Telegram : @Mr_aryan_a_h_a
+Link Gap : https://rubika.ir/my_gard_megatrn
+Ye Ostoreh
+"""
+
+time.sleep(4.3)
+print(f"{green} ")
+print (" Bye Bye Majazi  ")
+print (Fore.YELLOW + "")
+time.sleep(1.7)
+print ("                    I Love You yasi!mn 💜")
+print ("                    Godrat Majazi Mnm")
+print ("                    milad megatrn 💙 ")
+print(f"{blue} ")
+time.sleep(1.0)
+print ("")
+time.sleep(0.7)
+print ("   Error")
+time.sleep(0.6)
+print ("   The End")
+time.sleep(0.5)
+print ("   Bye Bye ")
+time.sleep(0.4)
+print ("   Finisheed")
+time.sleep(0.3)
+print ("   the megatrn")
+time.sleep(0.2)
+print ("   Tank You")
+time.sleep(0.1)
+print ("   I  Love")
+time.sleep(0.5)
+print ("   Mehrab")
+time.sleep(0.1)
+print ("   ")
+time.sleep(0.2)
+print ("   Tank You")
+time.sleep(0.1)
+print ("   I  Love")
+time.sleep(0.5)
+print ("   Mehrab")
+time.sleep(0.1)
+print ("   M bye") bye")
+print ("")
+time.sleep(1)
+print(f"{red} ")
+time.sleep(0.2)
+print (""" !!! Update Script !!!
+rm -rf filtring-rubika-megatrn
+git clone https://github.com/aryanmegatrn/filtring-rubika-megatrn
+cd filtring-rubika-megatrn
+chmod 777 *
+python filtring.py
+""")
